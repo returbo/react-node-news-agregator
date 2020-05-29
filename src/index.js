1c3ca72af35d6f26dc5489f94f1c09a941fcd9a7
returbo <returbo@mail.ru>
2020-05-29 11:34:20 +1000

    1 stage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+import './index.css';
+
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
