3744c9fccbb13875956a1c28760a3655459edd41
returbo <returbo@mail.ru>
2020-06-10 11:42:36 +1000

    parseLinks + parsePost working

--- server/index.js
+++ server/index.js
@@ -1,13 +1,43 @@
-import parsePost from './parsePost';
+import { parseLinks, getPosts, parsePost } from './parsePost';
+import fs from 'fs';
 import { elems } from './configs';
 
-parsePost(
-    'https://www.riadagestan.ru/news/education/v_detdomakh_dagestana_otmetili_mezhdunarodnyy_den_zashchity_detey/', 
-    elems.riadagestan
-    );
 
-parsePost(
-    'http://magastimes.ru/ingushskie-sudby-v-ekspozicii-gosudarstvennogo-muzeya-istorii-gulaga/',
-    elems.magastimes
-);
 
+
+
+
+
+
+// const saveResult = (json) => {
+//     fs.writeFile('result.json', json, (err) => {
+//         if (err) console.log('NOT SAVED!')
+//     });
+// };
+
+// const urlPage = 'http://magastimes.ru';
+
+// parseLinks(urlPage, '.td-module-title a')
+// .then(links => {
+//     getPosts(links).then(posts => saveResult(posts));
+// }).catch(e => console.log(e));
+
+
+
+
+// --- Работает ---
+
+// parseLinks('http://magastimes.ru', '.td_block_16 .td-module-title a').then(links => console.log(links));
+
+// parseLinks('https://www.riadagestan.ru', '.b-mid-col__layout li a' ).then(links => console.log(links));
+
+// --- Работает ---
+
+// parsePost(
+//     'http://magastimes.ru/i-vremenem-ne-vylechit-toj-boli-s-kakoj-smotreli-gory-gorcam-vsled/',
+//     elems.magastimes
+// ).then(post => console.log(post));
+
+// parsePost('https://www.riadagestan.ru/news/education/v_detdomakh_dagestana_otmetili_mezhdunarodnyy_den_zashchity_detey/',
+//     elems.riadagestan
+// ).then(post => console.log(post));
--- server/parsePost.js
+++ server/parsePost.js
@@ -1,30 +1,88 @@
 import unirest from "unirest";
 import cheerio from "cheerio";
+import { elems } from './configs';
 
+const log = (i, count, ms) => new Promise(r => setTimeout(() => {
+    console.log(`
+        ИНДЕКС: ${i};
+        Всего записей: ${count}
+    `)
+}, ms));
 
- function parsePost(url, { title, image, attr, text, views }) {
-    unirest.get(url).end( ( { body } ) => {
+function parsePost(url, { title, image, attr, text, views }) {
+    return new Promise((resolve, reject) => {
 
-        const $ = cheerio.load(body);
+        unirest.get(url).end(({ body, error }) => {
+            if (error) reject(error);
 
-        const parseDomain = url.match(/\/\/(.*?)\//)[1];
-        const parseTitle = $(title).text().trim();
-        let parseImage = $(image).attr(attr);
-        parseImage = parseImage.indexOf('http') >= 0 
-            ? parseImage 
-            : `http://${parseDomain}/${parseImage}`;
-        const parseText = $(text).text().trim().replace(/[\n+\t]/g, '');
-        const parseViews = $(views).text().trim();
+            const $ = cheerio.load(body);
 
-        const post = {
-            parseTitle,
-            parseImage,
-            parseText,
-            parseViews,
-        }
+            const parseDomain = url.match(/\/\/(.*?)\//)[1];
+            const parseTitle = $(title).text().trim();
+            let parseImage = $(image).attr(attr);
+            parseImage = parseImage.indexOf('http') >= 0
+                ? parseImage
+                : `http://${parseDomain}/${parseImage}`;
+            const parseText = $(text).text().trim().replace(/[\n+\t]/g, '');
+            const parseViews = $(views).text().trim();
 
-        console.log(post);
+            const post = {
+                parseTitle,
+                parseImage,
+                parseText,
+                parseViews,
+            }
+
+            resolve(post);
+        });
     });
 }
 
-export default parsePost;
+function parseLinks(url, className, maxLinks = 5) {
+    return new Promise((resolve, reject) => {
+        let links = [];
+
+        unirest.get(url).end(({ body, error }) => {
+            if (error) reject(error);
+
+            const $ = cheerio.load(body);
+            const parseDomain = url.match(/[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,9}/)[0];
+
+            $(className).each((i, e) => {
+                if (i + 1 <= maxLinks) {
+                    let link = $(e).attr('href');
+                    link = link.indexOf('http') >= 0
+                        ? link
+                        : `http://${parseDomain}${link}`;
+                    links.push(link);
+                }
+            });
+
+            resolve(links);
+            if (!links.length) reject({ error: 'empty' });
+        });
+    });
+}
+
+async function getPosts(links) {
+    return new Promise(async (resolve, reject) => {
+
+        let posts = [];
+        let count = links.length;
+
+        for (let i = 0; i < count; i++) {
+            const post = await parsePost(
+                links[i],
+                elems.riadagestan
+            ).then(post => post);
+            posts.push(post);
+            await log(i, count, 2000);
+        }
+
+        if (!posts.length) reject({ error: 'empty' });
+        resolve(posts);
+    })
+}
+
+
+export { parsePost, parseLinks, getPosts }
